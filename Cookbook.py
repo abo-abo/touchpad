4aed1ceb5b6d3536a81555fde8792f22fabefb61
Oleh Krehel <ohwoeowho@gmail.com>
2020-07-18 18:29:11 +0200

    Cookbook.py: Update

--- Cookbook.py
+++ Cookbook.py
@@ -1,14 +1,6 @@
 #* Imports
-import pycook.recipes.pip as pip
-from pycook.recipes.pip import clean, sdist
+from pycook.recipes.pip import clean, sdist, reinstall
 
 #* Recipes
-def pip_reinstall(recipe):
-    res = []
-    if pip.package_installed_p("touchpad"):
-        res += [pip.uninstall("touchpad")]
-    res += [pip.install(".")]
-    return res
-
 def publish(recipe):
     return sdist(recipe) + ["twine upload dist/*"] + clean(recipe)
